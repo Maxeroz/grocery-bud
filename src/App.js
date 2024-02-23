aaacbe48614ad6139339f56976e12a55f991a70d
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-23 22:05:56 +0400

    Clear List

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     } else if (name && isEditing) {
       // deal with edit
     } else {
-      // show alert
+      showAlert(true, "success", "item added to the list");
       const newItem = {
         id: new Date().getTime().toString(),
         title: name,
@@ -34,6 +34,11 @@ function App() {
 
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
   };
 
   return (
@@ -57,7 +62,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
