041a6e7b1c62e920cf87caa7f49618c8958fbe50
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-25 22:15:36 +0400

    Grocery Bud Local Storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  const list = localStorage.getItem("list");
+
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditId] = useState(null);
   const [alert, setAlert] = useState({
@@ -61,6 +71,10 @@ function App() {
     setEditId(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className="section-center">
